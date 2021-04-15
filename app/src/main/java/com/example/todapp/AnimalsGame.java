7a9bdf07f3bed1436dacff13fb786c1e6be8bf46
DeanKamel <odean1@hotmail.com>
2021-04-15 06:00:28 -0500

    Animals

--- app/src/main/java/com/example/todapp/AnimalsGame.java
+++ app/src/main/java/com/example/todapp/AnimalsGame.java
@@ -2,7 +2,12 @@ package com.example.todapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class AnimalsGame extends AppCompatActivity {
 
@@ -10,5 +15,34 @@ public class AnimalsGame extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animals_game);
+
+        Button levelOne = findViewById(R.id.level1_animals);
+        levelOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), AnimalsLevelOne.class);
+                startActivity(intent);
+            }
+        });
+
+        Button levelTwo = findViewById(R.id.level2_animals);
+        levelTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), AnimalsLevelTwo.class);
+                startActivity(intent);
+            }
+        });
+
+
+//        final MediaPlayer frog = MediaPlayer.create(this, R.raw.frog);
+//        ImageButton f = findViewById(R.id.frog);
+//        f.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                frog.start();
+//            }
+//        });
+
     }
 }
